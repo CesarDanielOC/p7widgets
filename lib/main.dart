157c9216b72d5718c1ceaf6ba0c3a3dddb765270
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2025-02-20 13:35:43 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiWidgets extends StatelessWidget {
+  const MiWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Olave",
+              style: TextStyle(color: Color(0xffffffff), fontSize: 30),
+            ),
+            backgroundColor: Color(0xffe3a55d),
+            centerTitle: true,
+          ),
+          body: (const Column(
+            children: <Widget>[
+              Text(
+                'Olave Cruz',
+                style: TextStyle(color: Color(0xff0d0050), fontSize: 30),
+              ),
+              Text(
+                "Mat:21308051280458 GPO 6 J",
+                style: TextStyle(color: Color(0x7e9a6e11), fontSize: 25),
+              ),
+              Expanded(
+                child: FittedBox(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          ))),
     );
-  }
-}
+  } //Build
+} //Widgets
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
